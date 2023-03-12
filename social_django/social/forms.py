1b2d7b123c41cee7fb1443687e17a859089e2901
AgustinDemicheli <agusmatadordemicheli@gmail.com>
2023-03-12 13:18:23 -0300

    version 2.0

--- social_django/social/forms.py
+++ social_django/social/forms.py
@@ -7,19 +7,14 @@ class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
 	password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
 	password2 = forms.CharField(label='Confirma Contraseña', widget=forms.PasswordInput)
-	#agregar photo=forms.Imagefield
+	
 
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 		help_texts = {k:"" for k in fields }
 
-#  class PostForm(models.form)
-# 	content = forms.CharField(label='', widget=forms.Textarea(attrs={'rows':2, 'placeholder': 'Publica tu receta ¡ incluye debajo una foto!'}), required=True)
-#	image= forms.ImageField()
-#	class Meta:
-#		model = Post
-#		fields = ['content','image']
+
 
 class PostForm(forms.ModelForm):
 	class Meta:
@@ -35,3 +30,13 @@ class PostForm(forms.ModelForm):
 			'title':forms.TextInput(attrs={'placeholder':'Ponle un nombre '}),
 			'content':forms.TextInput(attrs={'placeholder':'Publica una receta ¡No olvides la foto!'})
 		}
+
+class UserEditForm(UserCreationForm):
+	email=forms.EmailField(label="Modificar el Email")
+	password1=forms.CharField(label="contraseña",widget=forms.PasswordInput)
+	password2=forms.CharField(label="Repetir la contraseña",widget=forms.PasswordInput)
+
+	class Meta:
+		model= User
+		fields=['email','password1','password2']
+		help_texts={k:"" for k in fields}
